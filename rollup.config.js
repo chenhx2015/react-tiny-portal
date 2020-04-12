37a6852f8fe7bb14296b0da399a4c0be7cd230ed
chenhx2015 <chenhx9211@163.com>
2020-04-12 21:11:19 +0800

    add index.js

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
 export default {
-  input: 'src/portal.js' ,
+  input: 'src/index.js' ,
   output: [ { file: pkg.module,format: 'esm'}, 
             { file: pkg.main, format: 'umd' },
   ],
---
+++ src/index.js
@@ -0,0 +1,4 @@
+
+export * from './modal'
+
+export { default } from './usePortal'
